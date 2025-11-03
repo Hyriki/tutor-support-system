1827e0429815a4ef0e9eb025d453ac35a0e5a609
Hyriki <realhyriki@gmail.com>
2025-11-03 17:46:30 +0700

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+/**********   ADD PAGE ROUTE HERE   **********/
+import PrivateStorage from './pages/Resource/PrivateStorage'
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<PrivateStorage/>} />
+      <Route path="/about" element={<h1>About Page</h1>} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Resource/PrivateStorage.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { Link, useNavigate } from "react-router-dom";
+const PrivateStorage = () => {
+
+    return (
+        <div className="">
+            <Header />
+            
+            <Footer />
+        </div>
+    )
+}
+export default PrivateStorage;
