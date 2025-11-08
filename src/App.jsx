266722ce69469d1f4c8096a77100048a71f7a926
luthuanhung <luthuanhung2005@gmail.com>
2025-11-08 19:20:18 +0700

    First commit before meeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 /**********   ADD PAGE ROUTE HERE   **********/
 import PrivateStorage from './pages/Resource/PrivateStorage'
+import MyTestPage from './pages/Evaluation/MyTestPage'
+import StudentProfilePage from './pages/Evaluation/StudentProfilePage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PrivateStorage/>} />
       <Route path="/about" element={<h1>About Page</h1>} />
+
+      //! T.Hung below
+      <Route path="/my-test" element={<MyTestPage />} />
+      <Route 
+        path="/student-profile/:studentId" 
+        element={<StudentProfilePage />} 
+      />
+      //! T.Hung over
     </Routes>
   )
 }
